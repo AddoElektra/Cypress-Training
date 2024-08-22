a9e5340f68c37f8588855a5e10ba19294bd1f2fc
sayani <sayani.pramanik004@gmail.com>
2024-08-22 12:53:57 +0530

    Initial commit

---
+++ cypress/e2e/day1.cy.js
@@ -0,0 +1,26 @@
+/*describe('template spec', () => {
+    it('passes', () => {
+      cy.visit('www.nykaa.com')
+      cy.get('div[class="active css-1xt6clv"]').type('kajal').type('{enter}')
+      cy.get('span[class="css-1upamjb"]').should('have.text', 'Showing 20 of 403 results for ')
+    })
+  })*/
+
+  describe('template spec', () => {
+    it('test1', () => {
+      cy.visit('www.flipkart.com')
+      cy.get('input[class="Pke_EE"]').click().type('iPhone15')
+      cy.get('svg[xmlns="http://www.w3.org/2000/svg"]').click()
+    })
+  })
+
+  describe('template spec', () => {
+    it('test2', () => {
+      cy.visit('www.samsung.com/in/')
+      cy.get('button[class="nv00-gnb__utility-btn gnb__search-btn-js"]').click()
+      cy.get('input[id="gnb-search-keyword"]').type('Galaxy s24 Ultra')
+      cy.get('button[class="gnb-search__input-btn--search"]').click()
+    })
+  })
+
+    
---
+++ cypress/support/commands.js
@@ -0,0 +1,30 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+//
+//Cypress.on('window:before:load', (win) => {
+  //  win.navigator.__defineGetter__('userAgent', () => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
+//})
